b2a00b44bc55a7cd4117a45ed8298660159e535b
Ivan <63239064+Balleev-Ivan@users.noreply.github.com>
2022-09-02 13:13:57 +0400

    Update task1.py
    Добавил код

--- task1/task1.py
+++ task1/task1.py
@@ -1,1 +1,12 @@
-
+import sys
+nn = sys.argv[1]
+mm = sys.argv[2]
+n=int(nn)
+m=int(mm)
+i = 1
+while True:
+    print(i, end='')
+    i = 1 + (i + m - 2) % n
+    if i == 1:
+        break
+print(sep = '\n')
